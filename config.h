3abc39555a6cc80694720c93080c89136966ca8d
CronyAkatsuki <crolinuxgamer@gmail.com>
2021-04-29 19:58:59 +0200

    Updated settings.

--- config.h
+++ config.h
@@ -4,18 +4,18 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono Medium:size=8"
+	"JetBrains Mono Medium:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#fcfcfc", "#31363b" },
-	[SchemeSel] = { "#17a262", "#232627" },
-	[SchemeOut] = { "#2e3440", "#88c0d0" },
+	[SchemeNorm] = { "#f8f8f2", "#282a36" },
+	[SchemeSel] = { "#50fa7b", "#44475a" },
+	[SchemeOut] = { "#ffb86c", "#44475a" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 29;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 20;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
