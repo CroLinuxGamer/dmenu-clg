3da1b25ebb66a42ef7d5e6cd45aef31844cd08bf
CronyAkatsuki <cronyakatsuki@gmail.com>
2021-05-20 14:41:18 +0200

    Big update.

--- config.h
+++ config.h
@@ -9,13 +9,13 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#f8f8f2", "#282a36" },
-	[SchemeSel] = { "#f8f8f2", "#44475a" },
-	[SchemeOut] = { "#ffb86c", "#44475a" },
+	[SchemeNorm] = { "#cccccc", "#232121" },
+	[SchemeSel] = { "#f28c2a", "#232121" },
+	[SchemeOut] = { "#f28c2a", "#44475a" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 24;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 26;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
