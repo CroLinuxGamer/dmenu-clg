dee481ab8bb1b1b37c8624e053f2ae27051f892a
CronyAkatsuki <cronyakatsuki@gmail.com>
2021-05-22 13:09:09 +0200

    Basic version with autostart scripts and restart support.

--- config.h
+++ config.h
@@ -9,13 +9,13 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#cccccc", "#232121" },
-	[SchemeSel] = { "#f28c2a", "#232121" },
+	[SchemeNorm] = { "#D3D7CF", "#232121" },
+	[SchemeSel] = { "#F2F2F2", "#3465A4" },
 	[SchemeOut] = { "#f28c2a", "#44475a" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 26;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 10;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
